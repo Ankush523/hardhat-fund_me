ec67a2678eb0785928d0c101d270ac1c58a9d926
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-08 21:34:12 +0530

    deploy mocks

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,12 +1,31 @@
-//import 
+//import
 //main function
 //calling main function
-const {networkConfig} = require("../helper-hardhat-config")
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
 
-const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
 
-module.exports = async ({getNamedAccounts, deployments}) => {
-    const {deploy,log} = deployments
-    const {deployer} = await getNamedAccounts()
-    const chainId = network.config.chainId
-}
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress],
+    logs: true,
+  });
+};
+
+module.exports.tags = ["all","fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,7 @@ require("hardhat-deploy")
 module.exports = {
   solidity: {
     compilers:[{version: "0.8.8"},{version:"0.6.6"}],
-  }
+  },
+  defaultNetwork: "hardhat",
+ 
 };
